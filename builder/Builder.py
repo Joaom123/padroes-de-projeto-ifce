a5d4db20f216a2db7f6f69b5496461b64f14948e
João Marcus <joaomarcus12769@gmail.com>
2021-04-13 18:48:59 -0300

    Builder

---
+++ builder/Builder.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod, abstractproperty
+
+
+class Builder(ABC):
+    """
+    The Builder interface specifies methods for creating the different parts of
+    the Product objects.
+    """
+
+    @abstractproperty
+    def product(self) -> None:
+        pass
+
+    @abstractmethod
+    def produce_part_a(self) -> None:
+        pass
+
+    @abstractmethod
+    def produce_part_b(self) -> None:
+        pass
+
+    @abstractmethod
+    def produce_part_c(self) -> None:
+        pass
+
+
+
+
